3d68464eb0c8dba89a2376fdc73c41407a5a34ab
Atick Faisal <atickfaisal@gmail.com>
2023-10-15 11:10:02 +0300

    Update theme

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ const createWindow = () => {
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
+        frame: false
     });
 
     win.loadFile("index.html");
